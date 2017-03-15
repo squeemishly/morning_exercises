8520e0269d7a31b8e529d7bbb6051192628fe172
squeemishly <squeemishly@gmail.com>
2017-03-15 09:21:22 -0600

    first commit

---
+++ sum_of_squares
@@ -0,0 +1,3 @@
+numbers = (1..100).to_a
+
+p numbers
---
+++ sum_of_squares.rb
@@ -0,0 +1,32 @@
+numbers = (1..100).to_a
+i = 0
+x = 0
+sum_of_squares_total = 0
+square_of_sums_total = 0
+
+while i < numbers.length
+    numbers[i]=numbers[i]**2
+    i += 1
+end
+
+while x < numbers.length
+    sum_of_squares_total = sum_of_squares_total + numbers[x]
+    x += 1
+end
+
+puts "When you square each number 1 - 100 and add them together, you get #{sum_of_squares_total}!"
+
+numbers = (1..100).to_a
+i = 0
+x = 0
+
+while i < numbers.length
+    square_of_sums_total = square_of_sums_total + numbers[i]
+    i += 1
+end
+
+square_of_sums_total = square_of_sums_total ** 2
+
+puts "When you add each number 1 - 100 together, then square the total, you get #{square_of_sums_total}"
+
+puts "The difference between these two values is #{square_of_sums_total - sum_of_squares_total}"
