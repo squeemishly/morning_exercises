5f8a176ae51d84d9262676c15dfbacef80ae62e8
squeemishly <squeemishly@gmail.com>
2017-03-20 09:20:46 -0600

    as a class

--- leap_years.rb
+++ leap_years.rb
@@ -1,12 +1,16 @@
 year = 2017
 
-def find_leap_years(starting_date)
-  leap_years = []
-  until leap_years.length == 25
-    leap_years << starting_date if ((starting_date % 4 == 0) && (starting_date % 100 != 0)) || (starting_date % 400 == 0)
-    starting_date += 1
+class LeapYear
+
+  def find_leap_years(starting_date)
+    leap_years = []
+    until leap_years.length == 25
+      leap_years << starting_date if ((starting_date % 4 == 0) && (starting_date % 100 != 0)) || (starting_date % 400 == 0)
+      starting_date += 1
+    end
+    p leap_years
   end
-  leap_years
 end
 
-p find_leap_years(year)
+next_leap_years = LeapYear.new
+next_leap_years.find_leap_years(year)
