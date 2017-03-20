9b1fea6a4b7d961f7d7c1353641b625cd3c1304f
squeemishly <squeemishly@gmail.com>
2017-03-20 09:29:38 -0600

    added initialize. like a boss.

---
+++ leap_years2.rb
@@ -0,0 +1,20 @@
+puts "give me a year "
+year = gets.chomp.to_i
+
+class LeapYear
+  def initialize (starting_date)
+    @starting_date = starting_date
+  end
+
+  def find_leap_years(starting_date)
+    leap_years = []
+    until leap_years.length == 25
+      leap_years << starting_date if ((starting_date % 4 == 0) && (starting_date % 100 != 0)) || (starting_date % 400 == 0)
+      starting_date += 1
+    end
+    p leap_years
+  end
+end
+
+next_leap_years = LeapYear.new(year)
+next_leap_years.find_leap_years
