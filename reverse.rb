0a97a45c38fe9970222d532e65ec5548845adc05
squeemishly <squeemishly@gmail.com>
2017-03-21 20:56:20 -0600

    works as a class!

--- reverse.rb
+++ reverse.rb
@@ -1,38 +1,30 @@
-text = "super awesome string"
-split_text = []
-reverse_split_text = []
-reversed_text = ""
-
-split_text = text.split('')
-
-until split_text.length == 0
-  reverse_split_text << split_text.pop
-end
-
-reversed_text = reverse_split_text.join("")
-
-p text
-p reversed_text
-
 class ReverseText
   attr_accessor :text
   
   def initialize(text)
     @text = text
+    @reverse_split_text = []
   end
 
   def split_text
-    text.split('')
+    @text = text.split('')
   end
 
-  # def reverse_text(text)
-  #   until text.length == 0
-  #     reverse_split_text << split_text.pop
-  #   end
-  # end
+  def reverse_text
+    until @text.length == 0
+      @reverse_split_text << text.pop
+    end
+    @text = @reverse_split_text
+  end
+
+  def join_text
+    @text = @text.join('')
+  end
 
 end
 
 reverse = ReverseText.new(text)
 reverse.split_text
+reverse.reverse_text
+reverse.join_text
 p reverse.text
